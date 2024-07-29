3b54e6fabfffaa5c41c7d16c278458f3d59b9c54
toussaintmarvin <marvin.toussaint@gmail.com>
2024-07-29 01:50:37 -0400

    create main application file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'React-dom';
+import PatientRegistration from './components/PatientRegistration';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PatientRegistration />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
